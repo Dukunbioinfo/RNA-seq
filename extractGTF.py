3d2b7075d50a401138a1c1f8b7b7e461ad433957
Dukunbioinfo <48853431+Dukunbioinfo@users.noreply.github.com>
2019-03-23 19:25:52 +0800

    Add files via upload

---
+++ extractGTF.py
@@ -0,0 +1,19 @@
+
+##extract gtf file base on transcript name
+
+from argparse import ArgumentParser, FileType
+def extract_transcripts(gtf_file, transcript_file):
+    uixo = []
+    for i in transcript_file:
+        uixo.append(i.strip())
+    for i in gtf_file:
+        line = i.split()
+        if (line[9][1:-2]) in uixo:
+            print(i.strip())
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description= 'extract transcripts form a gtf file')
+    parser.add_argument('gtf_file',type=FileType('r'))
+    parser.add_argument('transcript_file',type=FileType('r'))
+    args = parser.parse_args()
+    extract_transcripts(args.gtf_file,args.transcript_file)
