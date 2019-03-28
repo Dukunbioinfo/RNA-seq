9b699e5f35951d815ede6a21a007e52e7206cc17
Dukunbioinfo <48853431+Dukunbioinfo@users.noreply.github.com>
2019-03-28 22:47:00 +0800

    Add files via upload

---
+++ interSect2Cols.py
@@ -0,0 +1,20 @@
+from argparse import ArgumentParser, FileType
+# fin1 = open(r'D:\rabbit_adyposite\diffExp\lncRNA\d3vsd0.MSTRG','r').readlines()
+# fin2 = open(r'D:\rabbit_adyposite\diffExp\lncRNA\d9vsd0.MSTRG','r').readlines()
+def unionMSTRG(fin1,fin2):
+    a=[]
+    for i in fin1:
+        a.append(i.strip())
+    b=[]
+    for i in fin2:
+        b.append(i.strip())
+    union= list(set(a).intersection(set(b)))
+    for i in union:
+        print(i)
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description= 'extract transcripts form a gtf file')
+    parser.add_argument('MSTRG1',type=FileType('r'))
+    parser.add_argument('MSTRG2',type=FileType('r'))
+    args = parser.parse_args()
+    unionMSTRG(args.MSTRG1,args.MSTRG2)
