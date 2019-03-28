a1f05715cf86a88e8321b118a67cb839323062d4
Dukunbioinfo <48853431+Dukunbioinfo@users.noreply.github.com>
2019-03-28 22:46:18 +0800

    Add files via upload

--- extractGTF.py
+++ extractGTF.py
@@ -7,13 +7,14 @@ def extract_transcripts(gtf_file, transcript_file):
     for i in transcript_file:
         uixo.append(i.strip())
     for i in gtf_file:
-        line = i.split()
-        if (line[9][1:-2]) in uixo:
-            print(i.strip())
+        if i[0]!='#':
+            line = i.split()
+            if (line[9][1:-2]) in uixo:
+                print(i.strip())
 
 if __name__ == '__main__':
     parser = ArgumentParser(description= 'extract transcripts form a gtf file')
     parser.add_argument('gtf_file',type=FileType('r'))
-    parser.add_argument('transcript_file',type=FileType('r'))
+    parser.add_argument('transcriptName_file',type=FileType('r'))
     args = parser.parse_args()
-    extract_transcripts(args.gtf_file,args.transcript_file)
+    extract_transcripts(args.gtf_file,args.transcriptName_file)
